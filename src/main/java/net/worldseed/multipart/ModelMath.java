646f97854c4e9fa8165b3f986529aed3e548ff82
iam4722202468 <aceparent@gmail.com>
2022-09-07 18:59:28 -0400

    Reduce allocations

--- src/main/java/net/worldseed/multipart/ModelMath.java
+++ src/main/java/net/worldseed/multipart/ModelMath.java
@@ -4,34 +4,42 @@ import net.minestom.server.coordinate.Point;
 import net.minestom.server.coordinate.Vec;
 
 class Matrix3 {
-    double[] x, y, z;
+    double x1, x2, x3;
+    double y1, y2, y3;
+    double z1, z2, z3;
 
     Matrix3(double x1, double x2, double x3, double y1, double y2, double y3, double z1, double z2, double z3) {
-        this.x = new double[]{x1, x2, x3};
-        this.y = new double[]{y1, y2, y3};
-        this.z = new double[]{z1, z2, z3};
+        this.x1 = x1;
+        this.x2 = x2;
+        this.x3 = x3;
+        this.y1 = y1;
+        this.y2 = y2;
+        this.y3 = y3;
+        this.z1 = z1;
+        this.z2 = z2;
+        this.z3 = z3;
     }
 
     Matrix3 mul(Matrix3 matrix3) {
-        double x1 = x[0] * matrix3.x[0] + x[1] * matrix3.y[0] + x[2] * matrix3.z[0];
-        double x2 = x[0] * matrix3.x[1] + x[1] * matrix3.y[1] + x[2] * matrix3.z[1];
-        double x3 = x[0] * matrix3.x[2] + x[1] * matrix3.y[2] + x[2] * matrix3.z[2];
+        double x1 = this.x1 * matrix3.x1 + this.x2 * matrix3.y1 + this.x3 * matrix3.z1;
+        double x2 = this.x1 * matrix3.x2 + this.x2 * matrix3.y2 + this.x3 * matrix3.z2;
+        double x3 = this.x1 * matrix3.x3 + this.x2 * matrix3.y3 + this.x3 * matrix3.z3;
 
-        double y1 = y[0] * matrix3.x[0] + y[1] * matrix3.y[0] + y[2] * matrix3.z[0];
-        double y2 = y[0] * matrix3.x[1] + y[1] * matrix3.y[1] + y[2] * matrix3.z[1];
-        double y3 = y[0] * matrix3.x[2] + y[1] * matrix3.y[2] + y[2] * matrix3.z[2];
+        double y1 = this.y1 * matrix3.x1 + this.y2 * matrix3.y1 + this.y3 * matrix3.z1;
+        double y2 = this.y1 * matrix3.x2 + this.y2 * matrix3.y2 + this.y3 * matrix3.z2;
+        double y3 = this.y1 * matrix3.x3 + this.y2 * matrix3.y3 + this.y3 * matrix3.z3;
 
-        double z1 = z[0] * matrix3.x[0] + z[1] * matrix3.y[0] + z[2] * matrix3.z[0];
-        double z2 = z[0] * matrix3.x[1] + z[1] * matrix3.y[1] + z[2] * matrix3.z[1];
-        double z3 = z[0] * matrix3.x[2] + z[1] * matrix3.y[2] + z[2] * matrix3.z[2];
+        double z1 = this.z1 * matrix3.x1 + this.z2 * matrix3.y1 + this.z3 * matrix3.z1;
+        double z2 = this.z1 * matrix3.x2 + this.z2 * matrix3.y2 + this.z3 * matrix3.z2;
+        double z3 = this.z1 * matrix3.x3 + this.z2 * matrix3.y3 + this.z3 * matrix3.z3;
 
         return new Matrix3(x1, x2, x3, y1, y2, y3, z1, z2, z3);
     }
 
     Point mul(Point vec) {
-        double vx = vec.x() * x[0] + vec.y() * x[1] + vec.z() * x[2];
-        double vy = vec.x() * y[0] + vec.y() * y[1] + vec.z() * y[2];
-        double vz = vec.x() * z[0] + vec.y() * z[1] + vec.z() * z[2];
+        double vx = vec.x() * x1 + vec.y() * x2 + vec.z() * x3;
+        double vy = vec.x() * y1 + vec.y() * y2 + vec.z() * y3;
+        double vz = vec.x() * z1 + vec.y() * z2 + vec.z() * z3;
 
         return new Vec(vx, vy, vz);
     }
