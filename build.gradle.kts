f1b58f5d3b2a23227942461cb7451b87dd258ff7
iam <iam4722202468@users.noreply.github.com>
2022-11-06 09:58:24 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ publishing {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    implementation("com.github.Minestom:Minestom:93718276c5")
+    implementation("com.github.Minestom:Minestom:42195c536b")
     implementation("commons-io:commons-io:2.11.0")
     implementation("org.zeroturnaround:zt-zip:1.8")
 
