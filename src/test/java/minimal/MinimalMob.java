64bd49f83666cb960c2d6e6239794bba7ae18a6c
iam4722202468 <aceparent@gmail.com>
2023-01-22 02:12:56 -0500

    Undo example changes

--- src/test/java/minimal/MinimalMob.java
+++ src/test/java/minimal/MinimalMob.java
@@ -1,14 +1,11 @@
 package minimal;
 
-import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.EntityCreature;
 import net.minestom.server.entity.EntityType;
 import net.minestom.server.instance.Instance;
-import net.minestom.server.timer.TaskSchedule;
 import net.worldseed.multipart.animations.AnimationHandler;
 import net.worldseed.multipart.animations.AnimationHandlerImpl;
-import net.worldseed.multipart.events.AnimationCompleteEvent;
 
 public class MinimalMob extends EntityCreature {
     private final Minimal model;
@@ -22,26 +19,7 @@ public class MinimalMob extends EntityCreature {
         model.init(instance, pos, this);
 
         this.animationHandler = new AnimationHandlerImpl(model);
-
-        MinecraftServer.getSchedulerManager().scheduleTask(() -> {
-            this.animationHandler.playOnce("opening", AnimationHandlerImpl.AnimationDirection.FORWARD, (a) -> {});
-        }, TaskSchedule.immediate(), TaskSchedule.tick(200));
-
-        MinecraftServer.getSchedulerManager().scheduleTask(() -> {
-            this.animationHandler.playOnce("opening", AnimationHandlerImpl.AnimationDirection.PAUSE, (a) -> {});
-        }, TaskSchedule.tick(50), TaskSchedule.tick(200));
-
-        MinecraftServer.getSchedulerManager().scheduleTask(() -> {
-            this.animationHandler.playOnce("opening", AnimationHandlerImpl.AnimationDirection.FORWARD, (a) -> {});
-        }, TaskSchedule.tick(100), TaskSchedule.tick(200));
-
-        MinecraftServer.getSchedulerManager().scheduleTask(() -> {
-            this.animationHandler.playOnce("opening", AnimationHandlerImpl.AnimationDirection.PAUSE, (a) -> {});
-        }, TaskSchedule.tick(150), TaskSchedule.tick(200));
-
-        this.eventNode().addListener(AnimationCompleteEvent.class, event -> {
-            System.out.println("Animation complete: " + event.animation() + " " + event.direction());
-        });
+        this.animationHandler.playRepeat("dab");
 
         this.setInstance(instance, pos);
     }
