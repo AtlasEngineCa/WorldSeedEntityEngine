e6632d6e1b2775cad589705ce1e4847e7b8e7970
iam4722202468 <aceparent@gmail.com>
2023-07-19 21:25:10 -0400

    Fix demo

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,8 @@ publishing {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    implementation("dev.hollowcube:minestom-ce:438338381e")
+    compileOnly("dev.hollowcube:minestom-ce:438338381e")
+    testImplementation("dev.hollowcube:minestom-ce:438338381e")
     implementation("commons-io:commons-io:2.11.0")
     implementation("org.zeroturnaround:zt-zip:1.8")
 
