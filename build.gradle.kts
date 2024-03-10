426e51ae4814a6c297ec5777d0ad892dccea46b8
iam <iam4722202468@users.noreply.github.com>
2024-03-10 00:15:50 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ publishing {
     publications.create<MavenPublication>("maven") {
         groupId = "net.worldseed.multipart"
         artifactId = "WorldSeedEntityEngine"
-        version = "10.1.3"
+        version = "10.4.0"
 
         from(components["java"])
     }
