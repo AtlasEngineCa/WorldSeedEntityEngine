22c36a7618248588481945e5a3c76a9b369b7ab8
iam4722202468 <aceparent@gmail.com>
2024-05-01 11:57:34 -0400

    fix animated textures

--- src/main/java/net/worldseed/resourcepack/multipart/generator/TextureGenerator.java
+++ src/main/java/net/worldseed/resourcepack/multipart/generator/TextureGenerator.java
@@ -1,9 +1,6 @@
 package net.worldseed.resourcepack.multipart.generator;
 
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonString;
-import javax.json.JsonValue;
+import javax.json.*;
 import java.util.Base64;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -24,6 +21,31 @@ public class TextureGenerator {
 
             if (uuid instanceof JsonString uid && mcmetas.containsKey(uid.getString())) {
                 mcmeta = mcmetas.get(uid.getString());
+            } else {
+                JsonValue frameTime = textureObj.get("frame_time");
+                JsonValue frame_interpolate = textureObj.get("frame_interpolate");
+
+                boolean hasValues = false;
+                JsonObjectBuilder builder = Json.createObjectBuilder();
+
+                if (frameTime instanceof JsonNumber number) {
+                    hasValues = true;
+                    builder.add("frametime", number);
+                }
+
+                try {
+                    if (frame_interpolate == JsonValue.FALSE) {
+                        hasValues = true;
+                        builder.add("interpolate", false);
+                    } else if (frame_interpolate == JsonValue.TRUE) {
+                        hasValues = true;
+                        builder.add("interpolate", true);
+                    }
+                } catch (Exception ignored) {}
+
+                if (hasValues) {
+                    mcmeta = Json.createObjectBuilder().add("animation", builder.build()).build();
+                }
             }
 
             textureMap.put(id, new TextureData(data, width, height, name, id, mcmeta));
