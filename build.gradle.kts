841dc55fce167bdbe816b16877f2d59c2a6f8acf
Zornov <zorinov.dev@gmail.com>
2025-06-07 13:44:04 +0000

    Revert version back by 0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ publishing {
     publications.create<MavenPublication>("maven") {
         groupId = "net.worldseed.multipart"
         artifactId = "WorldSeedEntityEngine"
-        version = "11.3.2"
+        version = "11.3.1"
 
         from(components["java"])
     }
