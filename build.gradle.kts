e2d86eab2ffd10fca163abfeb0ebf1f7af79de56
Alexander Parent <aceparent@gmail.com>
2025-07-13 18:09:51 -0400

    update to 1.21.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ publishing {
     publications.create<MavenPublication>("maven") {
         groupId = "net.worldseed.multipart"
         artifactId = "WorldSeedEntityEngine"
-        version = "11.3.1"
+        version = "11.3.2"
 
         from(components["java"])
     }
@@ -42,8 +42,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 
-    compileOnly("net.minestom:minestom-snapshots:1_21_5-6d70a6ca77")
-    testImplementation("net.minestom:minestom-snapshots:1_21_5-6d70a6ca77")
+    compileOnly("net.minestom:minestom:2025.07.11-1.21.7")
+    testImplementation("net.minestom:minestom:2025.07.11-1.21.7")
 
     implementation("commons-io:commons-io:2.11.0")
     implementation("org.zeroturnaround:zt-zip:1.8")
